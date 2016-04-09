ef8b0d226e17f12319608dd86d2c4c297e6b2f44
Fumikazu Fujiwara <github@ze.gs>
2016-04-09 19:40:42 +0900

    guzzle

--- index.php
+++ index.php
@@ -6,19 +6,15 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
 $app->post('/', function (Request $request, Response $response) {
-    error_log(__FILE__.":".__LINE__);
     $client = new GuzzleHttp\Client();
-    error_log(__FILE__.":".__LINE__);
 
     $body = json_decode($request->getBody(), true);
-    error_log(__FILE__.":".__LINE__);
 
     foreach ($body['result'] as $msg) {
-    error_log(__FILE__.":".__LINE__);
+        error_log(__FILE__.":".__LINE__.":".print_r($msg,true));
         $resContent = $msg['content'];
         $resContent['text'] = 'hello';
 
-    error_log(__FILE__.":".__LINE__);
         $requestOptions = [
             'body' => json_encode([
                 'to' => [$msg['content']['from']],
@@ -36,24 +32,21 @@ $app->post('/', function (Request $request, Response $response) {
                 'https' => getenv('FIXIE_URL'),
             ]
         ];
-    error_log(__FILE__.":".__LINE__);
 
         try {
             $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
         } catch (Exception $e) {
-            error_log($e->getMessage());
+            error_log(__FILE__.":".__LINE__.":".$e->getMessage());
         }
     }
 
     return $response;
 });
 $app->get('/', function (Request $request, Response $response) {
-    error_log(__FILE__.":".__LINE__);
     $response->getBody()->write(getenv('FIXIE_URL')."\n".
                 getenv('LINE_CHANNEL_ID')."\n".
                 getenv('LINE_CHANNEL_SECRET')."\n".
                 getenv('LINE_CHANNEL_MID'));
-    error_log(__FILE__.":".__LINE__);
     return $response;
 });
 $app->run();
