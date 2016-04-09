f6036c1ce15a9cf8497f4d8e05d39c27eaad30e2
Fumikazu Fujiwara <github@ze.gs>
2016-04-09 10:55:35 +0900

    try

--- index.php
+++ index.php
@@ -5,8 +5,38 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello, world");
+$app->post('/callback', function (Request $request, Response $response) {
+    $client = new GuzzleHttp\Client();
+
+    $body = json_decode($request->getContent(), true);
+
+        $resContent = $msg['content'];
+        $resContent['text'] = 'hello';
+
+        $requestOptions = [
+            'body' => json_encode([
+                'to' => [$msg['content']['from']],
+                'toChannel' => 1383378250, # Fixed value
+                'eventType' => '138311608800106203', # Fixed value
+                'content' => $resContent,
+            ]),
+            'headers' => [
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'X-Line-ChannelID' => getenv('LINE_CHANNEL_ID'),
+                'X-Line-ChannelSecret' => getenv('LINE_CHANNEL_SECRET'),
+                'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
+            ],
+            'proxy' => [
+                'https' => getenv('FIXIE_URL'),
+            ],
+        ];
+
+        try {
+            $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
 
     return $response;
 });
