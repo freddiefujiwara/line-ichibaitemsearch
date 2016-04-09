975d316fd924599e64e24f470968f9329cfb90da
Fumikazu Fujiwara <github@ze.gs>
2016-04-09 20:30:17 +0900

    guzzle

--- index.php
+++ index.php
@@ -6,15 +6,13 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
 $app->post('/', function (Request $request, Response $response) {
-    $client = new GuzzleHttp\Client();
-
     $body = json_decode($request->getBody(), true);
 
     foreach ($body['result'] as $msg) {
         $resContent = $msg['content'];
         $cli = new RakutenRws_Client();
-        $cli->setApplicationId('1030243823320196712');
-        $cli->setAffiliateId('0ca3304d.a811038d.0ca3304e.80024f1e');
+        $cli->setApplicationId(getenv('RAKUTEN_WEBSERVICE_APPLICATIN_ID'));
+        $cli->setAffiliateId(getenv('RAKUTEN_WEBSERVICE_AFFILIATE_ID'));
         $res = $cli->execute('IchibaItemSearch', array(
             'keyword' => $msg['content']['text'],
             'hits' => 3,
@@ -24,36 +22,36 @@ $app->post('/', function (Request $request, Response $response) {
             error_log(__FILE__.":".__LINE__.":".$res->getMessage());
             continue;
         }
-        $resContent['text'] = "";
         foreach ($res['Items'] as $item) {
             $resContent['text'] = "";
             $resContent['text'] .= $item['Item']['itemName']."\n";
             $resContent['text'] .= $item['Item']['itemCaption']."\n";
             $resContent['text'] .= $item['Item']['itemUrl'];
-        }
 
-        $requestOptions = [
-            'body' => json_encode([
-                'to' => [$msg['content']['from']],
-                'toChannel' => 1383378250, # Fixed value
-                'eventType' => '138311608800106203', # Fixed value
-                'content' => $resContent,
-            ]),
-            'headers' => [
-                'Content-Type' => 'application/json; charset=UTF-8',
-                'X-Line-ChannelID' => getenv('LINE_CHANNEL_ID'),
-                'X-Line-ChannelSecret' => getenv('LINE_CHANNEL_SECRET'),
-                'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
-            ],
-            'proxy' => [
-                'https' => getenv('FIXIE_URL'),
-                ]
-            ];
+            $requestOptions = [
+                'body' => json_encode([
+                    'to' => [$msg['content']['from']],
+                    'toChannel' => 1383378250, # Fixed value
+                    'eventType' => '138311608800106203', # Fixed value
+                    'content' => $resContent,
+                ]),
+                'headers' => [
+                    'Content-Type' => 'application/json; charset=UTF-8',
+                    'X-Line-ChannelID' => getenv('LINE_CHANNEL_ID'),
+                    'X-Line-ChannelSecret' => getenv('LINE_CHANNEL_SECRET'),
+                    'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
+                ],
+                'proxy' => [
+                    'https' => getenv('FIXIE_URL'),
+                    ]
+                ];
 
-        try {
-            $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
-        } catch (Exception $e) {
-            error_log(__FILE__.":".__LINE__.":".$e->getMessage());
+            $client = new GuzzleHttp\Client();
+            try {
+                $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
+            } catch (Exception $e) {
+                error_log(__FILE__.":".__LINE__.":".$e->getMessage());
+            }
         }
     }
 
@@ -67,4 +65,3 @@ $app->get('/', function (Request $request, Response $response) {
     return $response;
 });
 $app->run();
-
