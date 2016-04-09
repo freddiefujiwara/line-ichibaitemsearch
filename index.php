d3ae249743ad405e3d1baf78018d84f8e82571e2
Fumikazu Fujiwara <github@ze.gs>
2016-04-09 10:28:23 +0900

    change vendor file

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
-require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
